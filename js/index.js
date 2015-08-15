e7031227e403a14a46a1270b2a0c9a71fe5ab1e0
Spoorthy Vemula <sckortyman@gmail.com>
2015-08-14 18:55:29 -0700

    Increased size of elements on mobile

--- js/index.js
+++ js/index.js
@@ -4,16 +4,16 @@
 $(document).ready(function() {
 r = function() {
 if($(window).width() >= 960) {
-$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/p-copy-oval-1-copy-306.png' : 'images/p-copy-oval-1-copy-204.png') : 'images/p-copy-oval-1-copy-102-2.png');
+$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/p-copy-oval-1-copy-306.png' : 'images/p-copy-oval-1-copy-204.png') : 'images/p-copy-oval-1-copy-102.png');
 var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_1-933.png' : 'images/rsz_group_1-622.png') : 'images/rsz_group_1-311.png');
 var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_2-933.png' : 'images/rsz_group_2-622.png') : 'images/rsz_group_2-311.png');
 var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_4-933.png' : 'images/rsz_group_4-622.png') : 'images/rsz_group_4-311.png');
 $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/madewithsparkle-3-2.png' : 'images/madewithsparkle-2-2.png') : 'images/madewithsparkle-1-2.png');
 } else {
-$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/p-copy-oval-1-copy-102.png' : 'images/p-copy-oval-1-copy-68.png') : 'images/p-copy-oval-1-copy-34.png');
-var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_1-312.png' : 'images/rsz_group_1-208.png') : 'images/rsz_group_1-104.png');
-var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_2-312.png' : 'images/rsz_group_2-208.png') : 'images/rsz_group_2-104.png');
-var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_4-312.png' : 'images/rsz_group_4-208.png') : 'images/rsz_group_4-104.png');
+$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/p-copy-oval-1-copy-135.png' : 'images/p-copy-oval-1-copy-90.png') : 'images/p-copy-oval-1-copy-45.png');
+var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_1-600.png' : 'images/rsz_group_1-400.png') : 'images/rsz_group_1-200.png');
+var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_2-600.png' : 'images/rsz_group_2-400.png') : 'images/rsz_group_2-200.png');
+var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/rsz_group_4-600.png' : 'images/rsz_group_4-400.png') : 'images/rsz_group_4-200.png');
 $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/madewithsparkle-3.png' : 'images/madewithsparkle-2.png') : 'images/madewithsparkle-1.png');
 }
 };
